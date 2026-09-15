3c504ded5bf6721dbc5cba40bf5facdd79aaabde
mohamedMoharek <almharyqm107@gmail.com>
2026-09-15 22:08:38 +0300

    _

--- LMS_Backend/app.ts
+++ LMS_Backend/app.ts
@@ -52,9 +52,8 @@ const globalLimiter = rateLimit({
 app.use("/api", globalLimiter);
 
 const allowedOrigins = process.env.FRONTEND_URL 
-    ? [process.env.FRONTEND_URL, "http://localhost:5173", "http://localhost:3000"]
-    : ["http://localhost:5173", "http://localhost:3000"];
-    
+    ? [process.env.FRONTEND_URL, "http://localhost:5173", "http://localhost:3000", "http://192.168.1.4:5173"]
+    : ["http://localhost:5173", "http://localhost:3000", "http://192.168.1.4:5173"]; //يا عزب لو شفت دى فكرنى اشيل ال ip بتاع لابى من هنا قبل الرفع
 app.use(
     cors({
         origin: (origin, callback) => {
@@ -72,7 +71,8 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.static("public"));
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 // app.use(mongoSanitize());
 
 // API Routes
